cf513ef5ba7ba7ab1967c1aea7b640f760f345da
DJA <DJA@DJA-PC>
2018-01-24 17:38:35 +0900

    2018-01-24 bean을 활용하여 클래스 생성 첫 SpringProject 시작

---
+++ iot_spring/src/main/java/com/iot/spring/Excute.java
@@ -0,0 +1,21 @@
+package com.iot.spring;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+
+public class Excute {
+	public static void main(String[] args) {
+		Resource res = new ClassPathResource("beans.xml");
+		XmlBeanFactory bf = new XmlBeanFactory(res);
+		System.out.println("beans.xml파싱완료");
+		Test tt=(Test)bf.getBean("test");
+		tt.printTest();
+		System.out.println(tt);
+		tt=(Test)bf.getBean("test2");
+		tt.printTest();
+
+	}
+}
